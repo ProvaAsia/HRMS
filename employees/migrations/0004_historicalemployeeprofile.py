63024007d72fe8c571cc08e5c31a5b49fd969afb
Aristotoei <tjirawat@prova.fr>
2026-09-20 14:42:28 +0700

    fix: correct migration dependency for 0004_historicalemployeeprofile

--- employees/migrations/0004_historicalemployeeprofile.py
+++ employees/migrations/0004_historicalemployeeprofile.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('employees', '0003_employeedocument'),
+        ('employees', '0003_alter_employeeprofile_employee_status'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
